0587f27456dbc950292aa525e6c7143809ff48b4
Monika Lenartowicz <nika.nel@interia.pl>
2023-02-15 11:33:06 +0100

    Start changed app with architecture process: added class App with constructor and methods

--- script.js
+++ script.js
@@ -13,32 +13,53 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 let map, mapEvent;
 
-navigator.geolocation.getCurrentPosition(
-	function (position) {
+////NOTE CLASSES///
+class App {
+	#map;
+	#mapEvent;
+
+	constructor() {
+		this._getPosition(); //this na current object - zamiast: app._getPosition();
+	}
+
+	_getPosition() {
+		if (navigator.geolocation)
+			navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+				alert("Could not get your position");
+			});
+	}
+
+	_loadMap(position) {
 		const { latitude } = position.coords;
 		const { longitude } = position.coords;
 		console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
 		const coords = [latitude, longitude];
-
-		map = L.map("map").setView(coords, 13); //("map") =  <div id="map"></div> id z diva z html-a
+		console.log(this);
+		this.#map = L.map("map").setView(coords, 13); //("map") =  <div id="map"></div> id z diva z html-a
 		// console.log(map);
 
 		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		}).addTo(map);
+		}).addTo(this.#map);
 
 		//handling click on map
-		map.on("click", function (mapE) {
-			mapEvent = mapE;
+		this.#map.on("click", function (mapE) {
+			this.#mapEvent = mapE;
 			form.classList.remove("hidden");
 			inputDistance.focus();
 		});
-	},
-	function () {
-		alert("Could not get your position");
 	}
-);
+
+	_showForm() {}
+
+	_toggleElevationField() {}
+
+	_newWorkout() {}
+}
+
+const app = new App();
+// app._getPosition();
 
 //clear inputs fields
 const clearInputs = () => (inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = "");
